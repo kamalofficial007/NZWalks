a63652affc2a1e17c6ffbf5ee230dc8e71ad8f9e
kamalofficial007 <kamalkumar93@hotmail.com>
2023-02-15 12:37:52 +0530

    Create and update data into database using entityframework.

---
+++ NZWalksAPI/NZWalksAPI/Data/NZWalkDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalkDbContext:DbContext
+    {
+        public NZWalkDbContext(DbContextOptions<NZWalkDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+
+    }
+}
